1cad6805bd6528320c4f337a2ec0789ac0ac4201
HaoyangLiu <liuabel0318@gmail.com>
2025-12-29 18:14:19 +0800

    update

--- src/APP.tsx
+++ src/APP.tsx
@@ -3,6 +3,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { config } from './config/web3'
 import { SwapWidget } from './components/SwapWidget'
 import { ConnectButton } from './components/ConnectButton'
+import { useState, useCallback } from "react"
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
+import { SwapPanel } from "./components/panels/SwapPanel"
 
 const queryClient = new QueryClient()
 
